4ba361797254dfda1152f1e13667683996e59607
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-05-02 17:10:39 +0100

    Task 1. Basic annotations - concat Fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-""" Module documentation """
+
+'''
+Type-annotated function concat that takes a string str1 and a string str2 as
+arguments and returns their concatenation as a string.
+'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """Func doc"""
+    '''
+    Returns the concatenation of two strings
+
+    Parameters:
+        str1 : str
+        str2 : str
+
+    Returns:
+        str: concatenation of str1 and str2
+    '''
     return str1 + str2
