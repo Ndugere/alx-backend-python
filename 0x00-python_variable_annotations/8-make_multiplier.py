ab7497c3b31f3e04c45bd0945fbf0d90c51e3674
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-05-03 19:27:08 +0100

    Task 8. Complex types - functions Fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
-""" Module documentation """
+
+'''
+This module contains a function that returns a function that multiplies a float
+by a given multiplier.
+'''
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """Func doc"""
+    """
+    Return a function that multiplies a float by the given multiplier.
 
-    def mult(m: float) -> float:
-        """Func doc"""
-        return m * multiplier
+    Args:
+        multiplier (float): The multiplier to be used in the returned function.
 
-    return mult
+    Returns:
+        Callable[[float], float]: A function that takes a float as input and
+                                  returns the result of multiplying it by the
+                                  multiplier.
+    """
+    def multiplier_func(x: float) -> float:
+        """
+        Multiply a float by the given multiplier.
+
+        Args:
+            x (float): The input float.
+
+        Returns:
+            float: The result of multiplying the input float by the multiplier.
+        """
+        return x * multiplier
+
+    return multiplier_func
