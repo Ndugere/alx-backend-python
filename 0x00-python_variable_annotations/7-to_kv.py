05c51b13037c7089f21f824eef5b43d008611ee0
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2024-05-03 19:05:04 +0100

    Task 7. Complex types - string and int/float to tuple Fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
-""" Module documentation """
-from typing import Tuple, Union
+
+'''
+This function takes a string and a number and returns a tuple containing the
+string and the number squared.
+'''
+
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Func doc"""
-    return (k, v**2)
+    '''
+    This function takes a string and a number and returns a tuple containing
+    the string and the number squared.
+
+    Parameters:
+        k : str
+        v : int or float
+
+    Returns:
+        tuple[str, float]: a tuple containing the string and the number squared
+    '''
+    return (k, v ** 2.0)
