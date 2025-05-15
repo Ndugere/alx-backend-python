e6946077bc604d0074e28fe1c8d66c3278258f79
Your Name <you@example.com>
2025-05-15 20:08:45 +0300

    a function that uses a generator to fetch rows one by one from the user_data table.

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,14 +1,14 @@
 import mysql.connector
-
 from seed import connect_to_prodev
 
 def stream_users():
-   connnection = connect_to_prodev()
-   cursor = connnection.cursor()
-   cursor.execute("SELECT * FROM user_data")
+    connection = connect_to_prodev()
+    cursor = connection.cursor()
 
-   for row in rows:
-      yield row
-
-   cursor.close()
-   connnection.close()
+    try:
+        cursor.execute("SELECT * FROM user_data")
+        for row in cursor:
+            yield row
+    finally:
+        cursor.close()
+        connection.close()
