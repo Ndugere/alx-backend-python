4e3a9535169cccba07c92322bb8633cf26f1b22a
Your Name <you@example.com>
2025-05-19 21:41:33 +0300

    a decorator that logs database queries executed by any function

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -14,7 +14,7 @@ def log_queries(func):
     return wrapper
 @log_queries
 def fetch_all_users(query):
-    conn = sqlite3.connect('example.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
