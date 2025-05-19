e5ee29296dd635b40b3a6648e87ac591ee1a2830
Your Name <you@example.com>
2025-05-19 21:39:33 +0300

    a decorator that logs database queries executed by any function

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,24 @@
+import sqlite3
+import functools
+
+
+def log_queries(func):
+    def wrapper(*args, **kwargs):
+        query = None
+        if args:
+            query = args[0]
+        elif 'query' in kwargs:
+            query = kwargs['query']
+        print(f"[LOG] Executing SQL query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+users = fetch_all_users(query="SELECT * FROM users")
