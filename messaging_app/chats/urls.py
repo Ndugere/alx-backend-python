ada5e03cc54dd378e9c9225bf06198b5cf35508a
Your Name <you@example.com>
2025-05-29 22:02:13 +0300

    Added urls.py file for the app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet, StatusViewSet
 
-# Create a router and register our viewsets with it
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
-router.register(r'statuses', StatusViewSet, basename='status')  # Optional: only if you're using status updates
+router.register(r'statuses', StatusViewSet, basename='status')  # Optional
 
-# The API URLs are now determined automatically by the router
 urlpatterns = [
     path('', include(router.urls)),
 ]
