1ebc13b68c76ed6db9e17c539c56d508cd4b9dfd
Your Name <you@example.com>
2025-06-06 00:48:40 +0300

    added the settings.py to fit the chekcer's requirement

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'chats',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +120,17 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ],
+       'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 20,
+        ]
+
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
