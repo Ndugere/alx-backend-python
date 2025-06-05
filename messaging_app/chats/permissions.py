294f2262ce321a6f3a3feb254a24f86a42ade53e
Your Name <you@example.com>
2025-06-06 00:16:36 +0300

    Created and extended the permissions class IsParticipantOfConversation to ensure it Allow only authenticated users to access the api

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,19 +1,23 @@
 from rest_framework import permissions
 from .models import Conversation, Message
 
-class IsConversationParticipant(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Custom permission to allow only participants of a conversation to access it.
+    Custom permission to only allow participants of a conversation
+    to access or modify messages or conversations.
     """
+
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
+        # For Conversation objects
+        if isinstance(obj, Conversation):
+            return request.user in obj.participants.all()
+
+        # For Message objects
+        if isinstance(obj, Message):
+            return request.user in obj.conversation.participants.all()
+
+        return False
 
     def has_permission(self, request, view):
-        # This is optional unless you want to check permission before get_object() is called
-        return True
-class IsMessageSender(permissions.BasePermission):
-    """
-    Custom permission to allow only the sender of a message to update/delete it.
-    """
-    def has_object_permission(self, request, view, obj):
-        return obj.sender == request.user
+        # Only allow access if user is authenticated
+        return request.user and request.user.is_authenticated
